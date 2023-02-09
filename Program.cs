ce9545d23caf8a3fe4eb53f31afebc39e024e643
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-09 11:54:59 +0530

    Add : Solving using Switch Case Statement

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(" Welcome to Employee Wage Computation Program   ");
+            Console.WriteLine(" Welcome to Employee Wage Computation Program    ");
 
             EmployeeCheck.CheckEmployee();
 
