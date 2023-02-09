0b1ae0f367a80204a69b656a664158afda3c4816
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-09 11:41:55 +0530

    Add : Calculating Daily Employee Wage

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -11,17 +11,24 @@ namespace EmployeeWageComputationProblem
         public static void CheckEmployee()
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+
             Random random = new Random();
             int empCheck = random.Next(2);
 
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
 
         }  
     }
