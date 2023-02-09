8ced06a159979377bf1bd6895d63d178261f8d1b
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-09 11:47:48 +0530

    Add : Adding Part time Employee & Wages

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -12,16 +12,21 @@ namespace EmployeeWageComputationProblem
         {
             int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
+            int IS_PART_TIME = 2;
 
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
 
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
