ffc17ea101898602019578018ee31421511c78f4
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-09 11:55:33 +0530

    Add : Solving using Switch Case Statement

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -8,33 +8,33 @@ namespace EmployeeWageComputationProblem
 {
     public class EmployeeCheck
     {
+
+        public const int IS_FULL_TIME = 1;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int IS_PART_TIME = 2;
         public static void CheckEmployee()
         {
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
-            int IS_PART_TIME = 2;
-
             int empHrs = 0;
             int empWage = 0;
 
             Random random = new Random();
             int empCheck = random.Next(3);
 
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                empHrs = 8;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
-            }
+
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
-
         }  
     }
 }
